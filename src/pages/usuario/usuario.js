f7f0ed40f1c509eed902dd29c4b377bf56f3d653
LeydiMoon18 <98631662+LeydiMoon18@users.noreply.github.com>
2022-11-23 00:49:49 -0600

    carta, formulario y tabkle

--- src/pages/usuario/usuario.js
+++ src/pages/usuario/usuario.js
@@ -1,5 +1,17 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 const usuario = () =>{
-    return <h1>usuario</h1>
+    return <><h1>usuario</h1><Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+    </Card></>
 }
 
 export default usuario;
